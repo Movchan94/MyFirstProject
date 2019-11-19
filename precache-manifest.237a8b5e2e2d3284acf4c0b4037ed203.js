97e297ad39a199526a0b2bbddc20e23adc09ded2
unknown <you@example.com>
2019-11-19 14:26:08 +0200

    Updates

---
+++ precache-manifest.237a8b5e2e2d3284acf4c0b4037ed203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bedb73ae900f8770e47623f4a839ed",
+    "url": "/MyFirstProject/index.html"
+  },
+  {
+    "revision": "d28b46ec2fe4af2f7389",
+    "url": "/MyFirstProject/static/css/main.a89ea6a9.chunk.css"
+  },
+  {
+    "revision": "65120b9927202346b181",
+    "url": "/MyFirstProject/static/js/2.77eef704.chunk.js"
+  },
+  {
+    "revision": "d28b46ec2fe4af2f7389",
+    "url": "/MyFirstProject/static/js/main.f14d02a8.chunk.js"
+  },
+  {
+    "revision": "65e58802e94346f2fcb7",
+    "url": "/MyFirstProject/static/js/runtime-main.d7c59118.js"
+  },
+  {
+    "revision": "75ae2ba25d3d06d5079a940b79114940",
+    "url": "/MyFirstProject/static/media/SpinnerPreloader.75ae2ba2.gif"
+  }
+]);
